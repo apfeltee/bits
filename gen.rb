5494ad9d3e3b28d4c025eea72faf4a24527167a0
John Doe <root@localhost>
2016-09-24 02:04:52 +0200

    made gen.rb a little less awful

--- gen.rb
+++ gen.rb
@@ -1,16 +1,35 @@
 #!/usr/bin/ruby
-# this script is awful, just awful
+# still kinda awful
 
-entitydata = eval(File.read("entities.json"))
-$stdout << "static const struct{const char* key; const char* entity; const char* value; unsigned int length; unsigned int entlength;} html_entities[] = {\n"
+require "json"
+
+struct = %q<
+  struct htmlentities_entity
+  {
+    const char* key;
+    const char* entity;
+    const char* value;
+    unsigned int length;
+    unsigned int entlength;
+  }
+>
+
+# JSON.load doesn't seem to handle utf-8 properly for some reason
+# so we need to eval() the stuff
+entitydata = JSON.load(File.read("entities.json"))
+printf("static const %s html_entities[] = {\n", struct.strip)
 entitydata.each do |k, v|
   key = k.to_s
-  strval = v[:characters].to_s
-  $stdout << "    {" << key[1 .. -1][0 .. -2].dump << ", " << key.dump << ", \""
-  strval.each_byte do |ch|
-    $stdout << sprintf("\\x%x", ch)
-  end
-  $stdout << "\", #{strval.bytes.length}, #{key.length}},\n"
+  # get rid of '&' and ';', just get the blank entity name
+  keyname = key[1 .. -1][0 .. -2]
+  strval = v["characters"].to_s
+  printf(%q<    {%p, %p, "%s", %d, %d},> + "\n",
+    keyname,
+    key,
+    strval.bytes.map{|c| sprintf("\\x%x", c.ord) }.join,
+    strval.bytes.length,
+    key.length
+  )
 end
-$stdout << "    {0, 0, 0, 0, 0},\n"
-$stdout << "};\n"
+puts "    {0, 0, 0, 0, 0},"
+puts "};\n"
